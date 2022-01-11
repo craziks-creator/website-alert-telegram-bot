1b4a7670a7364d20735c5a2e6a967e5e1f47fb37
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-01-11 21:37:05 +0530

    Update request.py

--- bot/hf/request.py
+++ bot/hf/request.py
@@ -148,7 +148,7 @@ def notice_link(x, i, tree):
         return link
     except Exception as e:
         print(e)
-        return "http://dtu.ac.in/Web/notice/2021/april/file0428.pdf"
+        return "https://ssc.nic.in/SSCFileServer/PortalManagement/UploadedFiles/Write-up_(SI-CPO-20)_Paper-II_06012022_2.pdf"
 
 
 def dict_compare(d1, d2):
